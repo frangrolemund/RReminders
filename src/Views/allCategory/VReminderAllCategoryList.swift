81742dc37bbef32ce768592c6c7d26e5cfbedb6a
frangrolemund <frangrolemund@users.noreply.github.com>
2025-05-14 09:34:42 -0400

    initial layout for All view.

--- src/Views/allCategory/VReminderAllCategoryList.swift
+++ src/Views/allCategory/VReminderAllCategoryList.swift
@@ -8,21 +8,60 @@
 import SwiftUI
 
 struct VReminderAllCategoryList: View {
+	@Environment(VMReminderStore.self) private var modelData
+	
     var body: some View {
     	List {
-				Text("All")
-					.font(.largeTitle)
-					.bold()
-					.selectionDisabled()
-					.listRowSeparator(.hidden)
-					
-        		Text("TODO: items")
-        		
+			ForEach(modelData.lists) { list in
+				VReminderListSection(list, withDivider: list != modelData.lists[0])
+			}
+//			.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
 		}
 		.listStyle(.plain)
+		.navigationBarTitleDisplayMode(.large)
+		.navigationTitle("All")
     }
 }
 
+fileprivate struct VReminderListSection: View {
+	let list: VMReminderList
+	let hasDivider: Bool
+	
+	init(_ list: VMReminderList, withDivider: Bool) {
+		self.list = list
+		self.hasDivider = withDivider
+	}
+	
+	var body: some View {
+		Section {
+			ForEach(list.reminders) { reminder in
+				VReminderListItem(reminder: reminder, pendingReminder: .constant(nil))
+			}
+			
+		} header: {
+			VStack(alignment: .leading) {
+				if hasDivider {
+					Rectangle()
+						.frame(height: 2)
+						.foregroundStyle(Color.systemGray4)
+						.padding(.bottom, 10)
+				}
+			
+				Text(list.name)
+					.font(.title2)
+					.fontWeight(.bold)
+					.foregroundStyle(list.color.uiColor)
+					.padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+			}
+			.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+		}
+		.listSectionSeparator(.hidden)
+	}
+}
+
 #Preview {
-    VReminderAllCategoryList()
+	NavigationStack {
+    	VReminderAllCategoryList()
+	}
+	.environment(_PCReminderModel)
 }
